f2b595972abcf5323dddc5b8909b823df0d30b15
korewa456 <46043385+korewa456@users.noreply.github.com>
2023-07-08 17:07:34 -0700

    Added delete button for stores

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,24 +1,16 @@
 import prismadb from "@/lib/prismadb";
 
 interface DashboardPageProps {
-    params: { storeId: string}
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active store: {store?.name}</div>;
 };
 
-
-const DashboardPage: React.FC<DashboardPageProps> = async ({
-    params
-}) => 
-{
-    const store = await prismadb.store.findFirst({
-        where:{
-            id: params.storeId
-        }
-    });
-    return ( 
-    <div>
-        Active store: {store?.name}
-    </div> 
-    )
-}
- 
 export default DashboardPage;
--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,38 +1,38 @@
 import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
-    children,
-    params
+  children,
+  params,
 }: {
-    children: React.ReactNode;
-    params: { storeId: string }
+  children: React.ReactNode;
+  params: { storeId: string };
 }) {
-    const { userId } = auth();
+  const { userId } = auth();
 
-    if(!userId) {
-        redirect('/sign-in');
-    }
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId,
-            userId
-        }
-    });
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
 
-    if(!store) {
-        redirect('/');
-    }
+  if (!store) {
+    redirect("/");
+  }
 
-    return(
-        <>
-            <div>
-                <Navbar />
-            </div>
-            {children}
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      {children}
+    </>
+  );
 }
